a4b0f67129055c191a5502d77497d9180a300b1f
manuel21790 <email@manuel-roettger.com>
2025-04-25 11:56:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -13,4 +14,5 @@ def tipps():
     return res.json()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
